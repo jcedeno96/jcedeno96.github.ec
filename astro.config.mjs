56006b2b4ec440957e3e6d567b233a394365a535
jcedeno96 <jcedeno3547@utm.edu.ec>
2025-11-26 17:50:00 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: siteConfig.jcedeno96.github.ec,
     vite: {
         plugins: [tailwindcss()]
     },
