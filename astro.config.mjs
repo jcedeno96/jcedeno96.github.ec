d5bcf137834e877e6229484838b8b64cddec11cb
jcedeno96 <jcedeno3547@utm.edu.ec>
2025-11-26 18:11:38 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+import siteConfig from './src/data/site-config';
 
+/https://jcedeno96.github.ec/config
 export default defineConfig({
-  site: 'https://jcedeno96.github.ec',
-  base: 'mi-repo',
-})
+    site: siteConfig.website,
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [mdx(), sitemap()]
+});
